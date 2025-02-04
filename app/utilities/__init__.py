8cd1b9397d2164469a42cd4443f8a26cbfcd8d78
banhmioopla <qbingking@gmail.com>
2025-02-04 14:36:39 +0700

    fix something

---
+++ app/utilities/__init__.py
@@ -0,0 +1,8 @@
+from .broker_accounts import get_exness_mt5_accounts, get_vantage_mt5_accounts
+from .deals import get_nav
+
+__all__ = [
+    'get_exness_mt5_accounts',
+    'get_vantage_mt5_accounts'
+    'get_nav'
+]
---
+++ app/utilities/deals.py
@@ -0,0 +1,2 @@
+def get_nav():
+    pass
